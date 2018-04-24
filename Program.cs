6fb96f6801524f0ea1ba25caa4a211f6ef402530
joefroh <jmafrohlinger@outlook.com>
2018-04-24 12:12:12 -0700

    Adding console debugging temporarily while i build out logging

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace discordBot
@@ -19,16 +20,28 @@ namespace discordBot
         // async main
         public async Task MainAsync()
         {
+            Console.WriteLine("Starting up the bot.");
             _config = new Configuration();
             var token = _config["token"];
 
+            if (!String.IsNullOrEmpty(token))
+            {
+                Console.WriteLine("Got token.");
+            }
+
+            Console.WriteLine("Starting up client.");
             _client = new DiscordSocketClient();
             _client.Ready += ConnectedConfirm;
             _client.MessageReceived += MessageReceived;
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
-
+            Console.WriteLine("Awaiting.");
             await Task.Delay(-1);
+        }
+
+        private Task Connected()
+        {
+            throw new NotImplementedException();
         }
 
         // event handlers
@@ -37,12 +50,19 @@ namespace discordBot
         {
             if (arg.Author.IsBot != true)
             {
+                Console.WriteLine("Non Bot message received.");
                 await arg.Channel.SendMessageAsync("You said: " + arg.Content);
             }
         }
 
         private async Task ConnectedConfirm()
         {
+            Console.WriteLine("Connected as bot name: " + this._client.CurrentUser.Username);
+            foreach (var guild in this._client.Guilds)
+            {
+                Console.WriteLine("Seeing Guild: " + guild.Name);
+            }
+
             var enumerator = this._client.Guilds.GetEnumerator();
             enumerator.MoveNext();
 
@@ -53,6 +73,7 @@ namespace discordBot
             }
             var chan = enumerator.Current.GetTextChannel(channels.Current.Id);
             await chan.SendMessageAsync("Connected!");
+            Console.WriteLine("Bot is now ready to interact with users.");
         }
     }
 }
