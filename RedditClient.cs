b30717ada6b610697e00a24bbbf186e1696cf10a
joefroh <jmafrohlinger@outlook.com>
2018-04-26 16:02:47 -0700

    Adding new cache queue. there are some bugs in here somewhere...

--- RedditClient.cs
+++ RedditClient.cs
@@ -11,25 +11,55 @@ namespace discordBot
     {
         private RestClient _client;
         private Configuration _config;
+        private Queue<string> _recentPosts;
         public RedditClient(Configuration config)
         {
             _client = new RestClient("https://www.reddit.com");
             _config = config;
+            _recentPosts = new Queue<string>();
+
+            InitializeSubredditPosts();
         }
 
+        private void InitializeSubredditPosts()
+        {
+            var jobj = FetchSubredditNewSubmissions();
+            var tempStack = new Stack<string>();
+            foreach (var submission in jobj["data"]["children"])
+            {
+                tempStack.Push(submission["data"]["name"].ToString());
+            }
+
+            while (tempStack.Count != 0)
+            {
+                _recentPosts.Enqueue(tempStack.Pop());
+            }
+        }
+
+        private JObject FetchSubredditNewSubmissions()
+        {
+            var req = new RestRequest("/r/" + _config["TargetSubreddit"] + "/new.json");
+            req.AddParameter("raw_json", 1);
+            req.AddParameter("limit", 25);
+            var res = _client.Execute(req);
+            var jobj = JObject.Parse(res.Content);
+
+            return jobj;
+        }
         public IEnumerable<string> UpdateReddit()
         {
             var results = new List<string>();
-            var req = new RestRequest("/r/winnipegjets/new.json");
-            req.AddParameter("raw_json", 1);
-            req.AddParameter("limit", 5);
+            var jobj = FetchSubredditNewSubmissions();
 
-            var res = _client.Execute(req);
-            var obj = JsonConvert.DeserializeObject<ExpandoObject>(res.Content);
-            var jobj = JObject.Parse(res.Content);
             foreach (var submission in jobj["data"]["children"])
             {
-                results.Add(submission["data"]["title"].ToString() + ": " + submission["data"]["url"]);
+                if (!_recentPosts.Contains(submission["data"]["name"].ToString()))
+                {
+                    results.Add(submission["data"]["title"].ToString() + ": " + submission["data"]["url"]);
+                    if (_recentPosts.Count >= 25)
+                        _recentPosts.Dequeue();
+                    _recentPosts.Enqueue(submission["data"]["name"].ToString());
+                }
             }
 
             return results;
