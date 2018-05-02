b6e57f16f781ce9feaae343132af137931a55549
Joey Frohlinger <jmafrohlinger@outlook.com>
2018-05-01 21:23:29 -0700

    Refactoring configuration and boot up to funnel status messages to a single place

--- Configuration.cs
+++ Configuration.cs
@@ -8,6 +8,8 @@ namespace discordBot
         public bool EnableTextCommands { get; set; }
         public string CommandPrefix { get; set; }
         public int RedditRefreshTimer { get; set; }
+        public ulong AdminServerID { get; set; }
+        public ulong AdminChannelID { get; set; }
         public IEnumerable<SubredditConfig> SubredditConfig { get; set; }
     }
 }
