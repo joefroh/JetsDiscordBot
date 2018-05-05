4c40ad61d815e3afd7ccd2583019fcf67cf829e0
Joey Frohlinger <jmafrohlinger@outlook.com>
2018-05-05 12:45:08 -0700

    First rev of goal horn complete, sleeps till game

--- GoalDetail.cs
+++ GoalDetail.cs
@@ -5,5 +5,6 @@ namespace discordBot
         public int PlayIndex { get; set; }
         public string Description { get; set; }
         public string ScoringTeam { get; set; }
+        public int ScoringTeamId { get; set; }
     }
 }
