27ef2a921ee465555ab6114bfe3d2a8c4d9cf4bf
Joey Frohlinger <jmafrohlinger@outlook.com>
2018-05-12 12:07:12 -0700

    Some of the binding data types and the GetTeams methods

---
+++ NHLApi/Venue.cs
@@ -0,0 +1,10 @@
+namespace NHLApi
+{
+    public class Venue
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public string City { get; set; }
+        public TimeZone TimeZone { get; set; }
+    }
+}
