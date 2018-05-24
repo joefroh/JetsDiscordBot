70ab4d5f101b28f9c9a720eb0494c019159fc87a
joefroh <jmafrohlinger@outlook.com>
2018-05-24 16:54:23 -0700

    Adding GetCurrentRoster and associated data types

---
+++ NHLApi/Person.cs
@@ -0,0 +1,9 @@
+namespace NHLApi
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ NHLApi/Player.cs
@@ -0,0 +1,11 @@
+namespace NHLApi
+{
+    public class Player
+    {
+        public Person Person { get; set; }
+
+        public int JerseyNumber { get; set; }
+
+        public Position Position { get; set; }
+    }
+}
