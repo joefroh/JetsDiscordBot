2d4e5b39619cbeaa2f1f1d9dbf0b7849a3c30594
Joey Frohlinger <jmafrohlinger@outlook.com>
2018-05-26 15:50:50 -0700

    Adding Division and Conference APIs.

--- NHLApi/Division.cs
+++ NHLApi/Division.cs
@@ -5,5 +5,8 @@ namespace NHLApi
         public int Id { get; set; }
         public string Name { get; set; }
         public string Link { get; set; }
+        public string Abbreviation { get; set; }
+        public Conference Conference { get; set; }
+        public bool Active { get; set; }
     }
 }
