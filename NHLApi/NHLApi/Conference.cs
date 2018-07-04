bcd7eec618ed623e91af3e2137b7389b7222f91f
Joey Frohlinger <jmafrohlinger@outlook.com>
2018-07-04 12:26:37 -0700

    Added Conference and Division basic tests

--- NHLApi/NHLApi/Conference.cs
+++ NHLApi/NHLApi/Conference.cs
@@ -1,3 +1,6 @@
+using System;
+using Newtonsoft.Json;
+
 namespace NHLApi
 {
     public class Conference
@@ -8,5 +11,41 @@ namespace NHLApi
         public string Abbreviation { get; set; }
         public string ShortName { get; set; }
         public bool Active { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Conference);
+        }
+
+        public bool Equals(Conference p)
+        {
+            // If parameter is null, return false.
+            if (Object.ReferenceEquals(p, null))
+            {
+                return false;
+            }
+
+            // Optimization for a common success case.
+            if (Object.ReferenceEquals(this, p))
+            {
+                return true;
+            }
+
+            // If run-time types are not exactly the same, return false.
+            if (this.GetType() != p.GetType())
+            {
+                return false;
+            }
+
+            // Return true if the fields match.
+            // Note that the base class is not invoked because it is
+            // System.Object, which defines Equals as reference equality.
+            return JsonConvert.SerializeObject(this) == JsonConvert.SerializeObject(p);
+        }
+
+        public override int GetHashCode()
+        {
+            return JsonConvert.SerializeObject(this).GetHashCode();
+        }
     }
 }
