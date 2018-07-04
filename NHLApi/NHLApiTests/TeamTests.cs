abba65d1a83676a85ea3ed838a7cc85f803f00af
Joey Frohlinger <jmafrohlinger@outlook.com>
2018-07-03 21:23:18 -0700

    Passing GetTeamBasic test

--- NHLApi/NHLApiTests/TeamTests.cs
+++ NHLApi/NHLApiTests/TeamTests.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NHLApi;
 
 namespace NHLApiTests
@@ -7,10 +13,25 @@ namespace NHLApiTests
     public class TeamTests
     {
         private NHLApiClient api = new NHLApiClient();
+
         [TestMethod]
-        public void TestMethod1()
+        public void GetTeamsBasic()
         {
-            Assert.IsTrue(1 == 1);
+            // Load Expected result from file
+            var testResponse = File.ReadAllText(@"../../../TestAPIResponses/GetTeamsResult.json");
+            var jobj = JObject.Parse(testResponse);
+            var expected = JsonConvert.DeserializeObject<List<TeamDetail>>(jobj["teams"].ToString());
+
+            // Make API web call
+            List<TeamDetail> actual = api.GetTeams().ToList();
+
+            //Assert
+            Assert.AreEqual(expected.Count, actual.Count, "Expected is not equal to Actual length.");
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
         }
     }
 }
